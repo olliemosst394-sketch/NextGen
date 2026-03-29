bc349d27d378681d86687027edb3f1ba41d15c81
olliemosst394-sketch <olliemosst394@gmail.com>
2026-03-29 03:57:08 -0300

    Update PortfolioBalance.tsx

--- components/PortfolioBalance.tsx
+++ components/PortfolioBalance.tsx
@@ -14,6 +14,7 @@
 
 
 
+
 import { useAccount, useBalance, useReadContracts } from 'wagmi';
 import { formatEther, formatUnits } from 'viem';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
